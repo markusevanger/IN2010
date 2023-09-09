f162d3b25288bd20aee3c721136f231cc0281484
Markus <markusevanger@gmail.com>
2023-09-09 18:32:16 +0200

    hei

--- Implementasjoner/Sortering/MergeSort.py
+++ Implementasjoner/Sortering/MergeSort.py
@@ -25,21 +25,24 @@ def merge(A1, A2, A):
     return A
 
 
+# Tar en liste og splitter den i to, og rekursivt kaller mergeSort på hver splittet liste. 
+# Listen returneres kun når lengen av listen er 1. 
 def mergeSort(A):
 
     n = len(A)
-
-    if n <= 1:
-        return A
+    if n <= 1: # true når listen har blitt splittet nok til n = 1 
+        return A # avslutter reksursiv løkke
     
-    i = n//2
-    A1 = mergeSort(A[:i])
-    A2 = mergeSort(A[i:])
+    i = n//2 # finner midtpunktet av listen.
+
+    # kaller rekursivt på mergeSort 
+    A1 = mergeSort(A[:i]) # splicer listen i to, A[0...i-1]
+    A2 = mergeSort(A[i:]) # A[i...n]
     return merge(A1, A2, A)
 
 
 def main():
-    usortertListe = [4, 9, 2, 10, 5, 3, 6, 8, 7, 1, 11]
+    usortertListe = [4, 9, 2, 10, 5, 3, 6, 8, 7, 1]
 
     print("Her er den usorterte listen: ", usortertListe)
     print("Her er en merge sortert liste: ", mergeSort(usortertListe))
