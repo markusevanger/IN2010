a8403f8ec5c64bf9b97e7385b40feef81d45ee6d
markusevanger <markusevanger@gmail.com>
2023-09-14 15:32:09 +0200

    progress....

--- ObligKode/TequeList.py
+++ ObligKode/TequeList.py
@@ -1,27 +1,32 @@
 
-teque = []
 
-def push_back(x):
-    teque.append(x)
-def push_front(x):
-    teque.insert(0, x)
-def push_middle(x):
-    teque.insert((len(teque)+1) // 2, x)
-def get(i):
-    print(teque[int(i)])
 
-def main():
-    for n in range(int(input())):
-        S = input().split(" ")
-        x = S[1], S = S[0]
-        
-        if S == "push_back":
-            push_back(x)
-        if S == "push_front":
-            push_front(x)
-        if S == "push_middle":
-            push_middle(x)
-        if S == "get":
-            get(x)
+class TequeList:
 
-main()
+    def __init__(self):
+        self.teque = []
+
+    def push_back(self, x):
+        self.teque.append(x)
+    def push_front(self, x):
+        self.teque.insert(0, x)
+    def push_middle(self, x):
+        self.teque.insert((len(self.teque)+1) // 2, x)
+    def get(self, i):
+        print(self.teque[int(i)], end=", ")
+
+# def main():
+#     for n in range(int(input())):
+#         S = input().split(" ")
+#         x = S[1]
+#         S = S[0]
+#         
+#         if S == "push_back":
+#             push_back(x)
+#         if S == "push_front":
+#             push_front(x)
+#         if S == "push_middle":
+#             push_middle(x)
+#         if S == "get":
+#             get(x)
+
