99ff279ce40a686cb17694eb9adde560fbf39906
Markus <markusevanger@gmail.com>
2023-09-22 22:38:26 +0200

    setup for del 2 oblig 1

---
+++ ObligKode/oblig1/Sortering/insertion.py
@@ -0,0 +1,8 @@
+def sort(A):
+    # Do insertion sort here. Use the Sorter's comparison- and swap
+    # methods for automatically counting the swaps and comparisons.
+
+    # Use A.swap(i, j) to swap the values at two indices i and j. The swap is
+    # counted, when using this method. Comparisons are counted automatically.
+
+    return A
