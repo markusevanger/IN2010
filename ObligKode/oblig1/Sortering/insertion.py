79828484b960b83dc16222f49645e24d9ba38e15
markusevanger <markusevanger@gmail.com>
2023-09-23 12:56:09 +0200

    implement merge- & insertion sort

--- ObligKode/oblig1/Sortering/insertion.py
+++ ObligKode/oblig1/Sortering/insertion.py
@@ -5,4 +5,11 @@ def sort(A):
     # Use A.swap(i, j) to swap the values at two indices i and j. The swap is
     # counted, when using this method. Comparisons are counted automatically.
 
+    n = len(A)
+    for i in range(1, n):
+        j = i
+        while j > 0 and A[j-1] > A[j]:
+            A.swap(j-1, j)
+            j = j - 1
+
     return A
